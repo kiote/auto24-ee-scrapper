113385cfe4ad5ee2dc500ca524316674272719c0
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2019-01-27 13:24:31 +0200

    add docker deploy

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+npm version patch
+docker build --tag=local-scraper .
+version=$(cat package.json|  grep "version" | sed -e 's/"version"://g' | sed -e 's/"//g' | sed -e 's/,//g' | sed -e 's/ //g')
+echo "building $version"
+docker tag local-scraper:latest "kiote/scraper:$version"
+docker push "kiote/scraper:$version"
