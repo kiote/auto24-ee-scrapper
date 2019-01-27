dd2067eaea2ad54da35c055e7a4fc3dec28a8cd2
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2019-01-27 13:26:24 +0200

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,4 @@ version=$(cat package.json|  grep "version" | sed -e 's/"version"://g' | sed -e 
 echo "building $version"
 docker tag local-scraper:latest "kiote/scraper:$version"
 docker push "kiote/scraper:$version"
+git push --tags
