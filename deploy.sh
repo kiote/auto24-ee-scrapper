6e27b93eaa2d8e9b0dfad390617e5b5a4417b56f
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2019-02-10 10:43:46 +0200

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ docker build --tag=local-scraper .
 version=$(cat package.json|  grep "version" | sed -e 's/"version"://g' | sed -e 's/"//g' | sed -e 's/,//g' | sed -e 's/ //g')
 echo "building $version"
 docker tag local-scraper:latest "kiote/scraper:$version"
-docker push "kiote/scraper:$version"
+docker push "kiote/scraper:v$version"
 git push --tags
